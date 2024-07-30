7fe527b603d846a560f6ba04f74b0f1c718c116d
Joom <jnwongsa@gmail.com>
2024-07-30 10:44:25 +0700

    update: ex1-5

--- question-3.js
+++ question-3.js
@@ -1,20 +1,19 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
 Answer:
-
-
+O(logn) will fine the middle and start at the middle of the array and reduces the number of loop by half in each completed looped
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
 Answer:
-
+l(1) looped only one and will take the same amount of time even with the large inout
 
 */
 
 /* 
 Which function is more efficient and why?
 Answer:
-
+l(1) it better because the complexity time remain the same even with large input
 
 */
